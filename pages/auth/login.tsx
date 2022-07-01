69c196e5ad4430283a43e1009a40e30cebcc0631
Dalton <dalton.sterritt@gmail.com>
2022-07-01 00:35:45 -0600

    Added documentation for the "localUser"

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -9,6 +9,13 @@ import supabase from "@utils/supabaseClient";
 
 const Login: NextPage = () => {
   useEffect(() => {
+    /**
+     * If a user is logged in set a localStorage item called "localUser"
+     * with the user's id.
+     * This will be used to determine if a user is logged in or not.
+     * @warning This is not a secure way to do this: do not trust for protection purposes.
+     * @usecase This is used to determine if a user is logged in or not for displaying certain elements.
+     */
     const setUser = () => {
       const user = supabase.auth.user();
       if (!user) {
